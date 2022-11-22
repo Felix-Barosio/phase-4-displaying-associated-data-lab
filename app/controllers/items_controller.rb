2630d78974177ee3b0fbbe264b93b322ea50c803
Felix <barosiofelix@gmail.com>
2022-11-22 14:47:07 +0300

    Completed Assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+
+    def index
+
+        item = Item.all
+        render json: item, except: [:created_at, :updated_at], include: {user: {except: [:created_at, :updated_at]}}
+
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find_by(params[:id])
+        render json: user, except: [:created_at, :updated_at], include: {items: {except: [:user_id, :created_at, :updated_at]}}
+    end
 end
